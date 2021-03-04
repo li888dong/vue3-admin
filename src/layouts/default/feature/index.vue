ef6e4bd4948a6b9b682b6aae536106a7bc210c79
lidong <great888dong@163.com>
2021-03-04 11:47:46 +0800

    Initial commit

---
+++ src/layouts/default/feature/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <LayoutLockPage />
+  <BackTop v-if="getUseOpenBackTop" :target="getTarget" />
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import { createAsyncComponent } from '/@/utils/factory/createAsyncComponent';
+  import { BackTop } from 'ant-design-vue';
+  import { useRootSetting } from '/@/hooks/setting/useRootSetting';
+
+  export default defineComponent({
+    name: 'LayoutFeatures',
+    components: {
+      BackTop,
+      LayoutLockPage: createAsyncComponent(() => import('/@/views/sys/lock/index.vue')),
+    },
+    setup() {
+      const { getUseOpenBackTop } = useRootSetting();
+
+      return {
+        getTarget: () => document.body,
+        getUseOpenBackTop,
+      };
+    },
+  });
+</script>
---
+++ src/layouts/default/setting/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div @click="openDrawer" :class="prefixCls">
+    <Icon icon="ion:settings-outline" />
+    <SettingDrawer @register="register" />
+  </div>
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import SettingDrawer from './SettingDrawer';
+  import Icon from '/@/components/Icon';
+
+  import { useDrawer } from '/@/components/Drawer';
+  import { useDesign } from '/@/hooks/web/useDesign';
+
+  export default defineComponent({
+    name: 'SettingButton',
+    components: { SettingDrawer, Icon },
+    setup() {
+      const [register, { openDrawer }] = useDrawer();
+
+      const { prefixCls } = useDesign('setting-button');
+      return {
+        prefixCls,
+        register,
+        openDrawer,
+      };
+    },
+  });
+</script>
